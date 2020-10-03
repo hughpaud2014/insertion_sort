9122799e5f8a04e7ccc2862a78c1185c55009f90
Paul <hughpaud@gmail.com>
2020-10-03 17:38:18 -0500

    ::construction:: updated comments to explain algorithm.

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -3,12 +3,15 @@
 // Referenced from: "The Algorithm Design Manual" by Steven S. Skiena
 
 class InsertionSort {
+
+    // 1.) Define the InsertSort function
     void sort(int array[]) {
         int n = array.length;
         for (int i = 1; i < n; ++i) {
             int key = array[i];
             int j = i - 1;
 
+            // 2.) Move the elements of the array that are greater than the key.
             while (j >= 0 && array[j] > key) {
                 array[j + 1] = array[j];
                 j = j -1;
@@ -17,6 +20,7 @@ class InsertionSort {
         }
 
     }
+    // 3.) Create a utility function to print the array of size n
     static void printArray(int array[]) {
         int n = array.length;
         for (int i = 0; i < n; ++i) System.out.print ( array[i] + " " );
@@ -24,12 +28,10 @@ class InsertionSort {
     }
 
 
-
-
+    // 4.) The main function to output to console.
     public static void main(String args[]) {
 
         int array[] = { 10, 250, 3, 24, 35,};
-
         InsertionSort ob = new InsertionSort ();
         ob.sort(array);
         printArray ( array );
